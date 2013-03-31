3285f6034709d1d9ef660d2e4f629faf5dfde15a
Lucas Jenss <lucas@x3ro.de>
2013-03-31 13:56:12 +0200

    Add the JS folder

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    /*
+    $('pre').each(function() {
+        if(!this.className.match(/\bno-prettyprint\b/)) {
+            this.className += ' prettyprint';
+        }
+    });
+    */
+
+    prettyPrint();
+
+});
